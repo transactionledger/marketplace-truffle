316fd5870ec4b2d64f8a8ea1644a8616eff32050
Transaction Ledger <info@transactionledger.dev>
2019-08-27 20:23:51 -0400

    add create product function and product created event

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -13,7 +13,27 @@ contract Marketplace {
         bool purchased;
     }
 
+    event ProductCreated(
+        uint id,
+        string name,
+        uint price,
+        address owner,
+        bool purchased
+    );
+
     constructor() public {
         name = "Tech University Marketplace";
+    }
+
+    function createProduct(string memory _name, uint _price) public {
+        require(bytes(_name).length > 0, 'must have a name');
+        require(_price > 0, 'price must be greater than 0');
+
+        productCount++;
+
+        //create the product
+        products[productCount] = Product(productCount, _name, _price, msg.sender, false);
+
+        emit ProductCreated(productCount, _name, _price, msg.sender, false);
     }
 }

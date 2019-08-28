b17f2da2c1fb22d3cb14bf03f91a93ba2a937326
Transaction Ledger <info@transactionledger.dev>
2019-08-27 20:19:50 -0400

    add product struct and mapping and product count field

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -2,8 +2,18 @@ pragma solidity ^0.5.0;
 
 contract Marketplace {
     string public name;
+    mapping(uint => Product) public products;
+    uint public productCount = 0;
+
+    struct Product {
+        uint id;
+        string name;
+        uint price;
+        address owner;
+        bool purchased;
+    }
 
     constructor() public {
-        name = "Dapp University Marketplace";
+        name = "Tech University Marketplace";
     }
 }
